a3d8afb7bd49654945cb071b451218b1b91c659b
Coreen Cooper <coreencooper@pursuit.org>
2021-12-19 21:11:40 -0500

    add apiKey

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -9,18 +9,17 @@ const Recipe = () => {
   const { title, summary, image, sourceUrl } = recipe;
   const { id } = useParams();
 
-  const fetchRecipe = async () => {
-    try {
-      const res = await axios.get(
-        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=false`
-      );
-      setRecipe(res.data);
-    } catch (error) {
-      return error;
-    }
-  };
-
   useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=false`
+        );
+        setRecipe(res.data);
+      } catch (error) {
+        return error;
+      }
+    };
     fetchRecipe();
   }, [id]);
 
@@ -33,7 +32,7 @@ const Recipe = () => {
 
       <h2 className="recipe-title">{title}</h2>
       <p id="summary"> {convertSummary(summary)}</p>
-      
+
       <img src={image} alt="vegan" className="recipe-image" />
       <a href={sourceUrl} target="_blank" rel="noreferrer">
         Click for more information
